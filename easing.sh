a8316302ca886ae36495b29faaefc326aa8027f3
b3nson <benjamin@lafkon.net>
2014-09-25 17:23:25 +0200

    added License-Header and bc scale-value

--- easing.sh
+++ easing.sh
@@ -12,205 +12,209 @@ function assignvars {
 	p=$6
 }
 
-# LINEAR =============================================================
+
+# LINEAR
+# -------------------------------------------------------------------------- #
 function linear_easeIn {
 	assignvars $1 $2 $3 $4
-	echo "$c*$t/$d+$b" | bc -l
+	echo "$OPT $c*$t/$d+$b" | bc -l
 }
 
 function linear_easeOut {
 	assignvars $1 $2 $3 $4
-	echo "$c*$t/$d + $b" | bc -l
+	echo "$OPT $c*$t/$d + $b" | bc -l
 }
 
 function linear_easeInOut {
 	assignvars $1 $2 $3 $4
-	echo "$c*$t/$d + $b" | bc -l
+	echo "$OPT $c*$t/$d + $b" | bc -l
 }
 
-# SINE =============================================================
+# SINE
+# -------------------------------------------------------------------------- #
 function sine_easeIn {
 	assignvars $1 $2 $3 $4
-	echo "-1*$c * c($t/$d * ($PI/2)) + $c + $b" | bc -l
+	echo "$OPT -1*$c * c($t/$d * ($PI/2)) + $c + $b" | bc -l
 }
 function sine_easeOut {
 	assignvars $1 $2 $3 $4
-	echo "$c * s($t/$d * ($PI/2)) + $b" | bc -l
+	echo "$OPT $c * s($t/$d * ($PI/2)) + $b" | bc -l
 }
 function sine_easeInOut {
 	assignvars $1 $2 $3 $4
-	echo "-1*$c/2 * (c($PI*$t/$d) - 1) + $b" | bc -l
+	echo "$OPT -1*$c/2 * (c($PI*$t/$d) - 1) + $b" | bc -l
 }
 
-# QUAD =============================================================
+# QUAD
+# -------------------------------------------------------------------------- #
 function quad_easeIn {
 	assignvars $1 $2 $3 $4
-	t=`echo "$t/$d" | bc -l`
-	echo "$c*$t*$t + $b" | bc -l
+	t=`echo "$OPT $t/$d" | bc -l`
+	echo "$OPT $c*$t*$t + $b" | bc -l
 }
 function quad_easeOut {
 	assignvars $1 $2 $3 $4
-	t=`echo "$t/$d" | bc -l`
-	echo "-1*$c*$t*($t-2) + $b" | bc -l
+	t=`echo "$OPT $t/$d" | bc -l`
+	echo "$OPT -1*$c*$t*($t-2) + $b" | bc -l
 }
 function quad_easeInOut {
 	assignvars $1 $2 $3 $4
-	t=`echo "$t/($d/2)" | bc -l`
-	if [ `echo "$t < 1" | bc -l` -eq 1 ]
+	t=`echo "$OPT $t/($d/2)" | bc -l`
+	if [ `echo "$OPT $t < 1" | bc -l` -eq 1 ]
 	then 
-	  echo "$c/2*$t*$t + $b" | bc -l
+	  echo "$OPT $c/2*$t*$t + $b" | bc -l
 	else
-	  t=`echo "$t-1" | bc -l`
-	  echo "-1*$c/2*(($t*($t-2)) -1) + $b" | bc -l
+	  t=`echo "$OPT $t-1" | bc -l`
+	  echo "$OPT -1*$c/2*(($t*($t-2)) -1) + $b" | bc -l
 	fi
 }
 
-
-# CUBIC =============================================================
+# CUBIC
+# -------------------------------------------------------------------------- #
 function cubic_easeIn {
 	assignvars $1 $2 $3 $4
-	t=`echo "$t/$d" | bc -l`
-	echo "$c*$t*$t*$t + $b" | bc -l
+	t=`echo "$OPT $t/$d" | bc -l`
+	echo "$OPT $c*$t*$t*$t + $b" | bc -l
 }
 function cubic_easeOut {
 	assignvars $1 $2 $3 $4
-	t=`echo "$t/$d-1" | bc -l`
-	echo "$c*($t*$t*$t +1) + $b" | bc -l
+	t=`echo "$OPT $t/$d-1" | bc -l`
+	echo "$OPT $c*($t*$t*$t +1) + $b" | bc -l
 }
 function cubic_easeInOut {
 	assignvars $1 $2 $3 $4
-	t=`echo "$t/($d/2)" | bc -l`
-	if [ `echo "$t < 1" | bc -l` -eq 1 ]
+	t=`echo "$OPT $t/($d/2)" | bc -l`
+	if [ `echo "$OPT $t < 1" | bc -l` -eq 1 ]
 	then
-	  echo "$c/2*$t*$t*$t + $b" | bc -l
+	  echo "$OPT $c/2*$t*$t*$t + $b" | bc -l
 	else
-	  t=`echo "$t-2" | bc -l`
-	  echo "$c/2*($t*$t*$t + 2) + $b" | bc -l
+	  t=`echo "$OPT $t-2" | bc -l`
+	  echo "$OPT $c/2*($t*$t*$t + 2) + $b" | bc -l
 	fi
 }
 
-
-# QUART =============================================================
+# QUART
+# -------------------------------------------------------------------------- #
 function quart_easeIn {
 	assignvars $1 $2 $3 $4
-	t=`echo "$t/$d" | bc -l`
-	echo "$c*$t*$t*$t*$t + $b" | bc -l
+	t=`echo "$OPT $t/$d" | bc -l`
+	echo "$OPT $c*$t*$t*$t*$t + $b" | bc -l
 }
 function quart_easeOut {
 	assignvars $1 $2 $3 $4
-	t=`echo "$t/$d-1" | bc -l`
-	echo "-1*$c * ($t*$t*$t*$t - 1) + $b" | bc -l
+	t=`echo "$OPT $t/$d-1" | bc -l`
+	echo "$OPT -1*$c * ($t*$t*$t*$t - 1) + $b" | bc -l
 }
 function quart_easeInOut {
 	assignvars $1 $2 $3 $4
-	t=`echo "$t/($d/2)" | bc -l`
-	if [ `echo "$t < 1" | bc -l` -eq 1 ]
+	t=`echo "$OPT $t/($d/2)" | bc -l`
+	if [ `echo "$OPT $t < 1" | bc -l` -eq 1 ]
 	then
-	  echo "$c/2*$t*$t*$t*$t + $b" | bc -l
+	  echo "$OPT $c/2*$t*$t*$t*$t + $b" | bc -l
 	else
-	  t=`echo "$t-2" | bc -l`
-	  echo "-1*$c/2 * ($t*$t*$t*$t - 2) + $b" | bc -l
+	  t=`echo "$OPT $t-2" | bc -l`
+	  echo "$OPT -1*$c/2 * ($t*$t*$t*$t - 2) + $b" | bc -l
 	fi
 }
 
-
-# QUINT =============================================================
+# QUINT
+# -------------------------------------------------------------------------- #
 function quint_easeIn {
 	assignvars $1 $2 $3 $4
-	t=`echo "$t/$d" | bc -l`	
-	echo "$c*$t*$t*$t*$t*$t + $b" | bc -l
+	t=`echo "$OPT $t/$d" | bc -l`	
+	echo "$OPT $c*$t*$t*$t*$t*$t + $b" | bc -l
 }
 function quint_easeOut {
 	assignvars $1 $2 $3 $4
-	t=`echo "$t/$d-1" | bc -l`	
-	echo "$c*($t*$t*$t*$t*$t + 1) + $b" | bc -l
+	t=`echo "$OPT $t/$d-1" | bc -l`	
+	echo "$OPT $c*($t*$t*$t*$t*$t + 1) + $b" | bc -l
 }
 function quint_easeInOut {
 	assignvars $1 $2 $3 $4
-	t=`echo "$t/($d/2)" | bc -l`
-	if [ `echo "$t < 1" | bc -l` -eq 1 ]
+	t=`echo "$OPT $t/($d/2)" | bc -l`
+	if [ `echo "$OPT $t < 1" | bc -l` -eq 1 ]
 	then
-	  echo "$c/2*$t*$t*$t*$t + $b" | bc -l
+	  echo "$OPT $c/2*$t*$t*$t*$t + $b" | bc -l
 	else
-	  t=`echo "$t-2" | bc -l`
-	  echo "-1*$c/2 * ($t*$t*$t*$t - 2) + $b" | bc -l
+	  t=`echo "$OPT $t-2" | bc -l`
+	  echo "$OPT -1*$c/2 * ($t*$t*$t*$t - 2) + $b" | bc -l
 	fi
 }
 
-
-# EXPO =============================================================
+# EXPO
+# -------------------------------------------------------------------------- #
 function expo_easeIn {
 	assignvars $1 $2 $3 $4
-	if [ `echo "$t == 0" | bc -l` -eq 1 ]
+	if [ `echo "$OPT $t == 0" | bc -l` -eq 1 ]
 	then
 	  TMP=$b
 	else
 	  TMP=$c
 	fi
-	echo "$TMP * e( l(2) * (($t/$d -1)*10) ) + $b" | bc -l
+	echo "$OPT $TMP * e( l(2) * (($t/$d -1)*10) ) + $b" | bc -l
 }
 function expo_easeOut {
 	assignvars $1 $2 $3 $4
-	if [ `echo "$t == $d" | bc -l` -eq 1 ]
+	if [ `echo "$OPT $t == $d" | bc -l` -eq 1 ]
 	then
-	  TMP=`echo "$b+$c" | bc -l`
+	  TMP=`echo "$OPT $b+$c" | bc -l`
 	else
 	  TMP=$c
 	fi
-	echo "$TMP * ( -1*e( l(2)*(-10*$t/$d) ) +1  ) + $b" | bc -l
+	echo "$OPT $TMP * ( -1*e( l(2)*(-10*$t/$d) ) +1  ) + $b" | bc -l
 }
 function expo_easeInOut {
 	assignvars $1 $2 $3 $4
-	TMP=`echo "$t/($d/2)" | bc -l`
-	if [ `echo "$t == 0" | bc -l` -eq 1 ]
+	TMP=`echo "$OPT $t/($d/2)" | bc -l`
+	if [ `echo "$OPT $t == 0" | bc -l` -eq 1 ]
 	then
-	  echo "$b" | bc -l
-	elif [ `echo "$t == $d" | bc -l` -eq 1 ]
+	  echo "$OPT $b" | bc -l
+	elif [ `echo "$OPT $t == $d" | bc -l` -eq 1 ]
 	then
-	  echo "$b + $c" | bc -l
-	elif [ `echo "$TMP < 1" | bc -l` -eq 1 ]
+	  echo "$OPT $b + $c" | bc -l
+	elif [ `echo "$OPT $TMP < 1" | bc -l` -eq 1 ]
 	then
-	  echo "($c/2) * e( l(2)*(10*($TMP-1)) ) + $b" | bc -l
+	  echo "$OPT ($c/2) * e( l(2)*(10*($TMP-1)) ) + $b" | bc -l
 	else
-	  TMP=`echo "$TMP-1" | bc -l`
-	  echo "($c/2) * ( -1*e( l(2)*(-10*$TMP) ) +2 ) + $b" | bc -l
+	  TMP=`echo "$OPT $TMP-1" | bc -l`
+	  echo "$OPT ($c/2) * ( -1*e( l(2)*(-10*$TMP) ) +2 ) + $b" | bc -l
 	fi
 }
 
-
-# CIRC =============================================================
+# CIRC
+# -------------------------------------------------------------------------- #
 function circ_easeIn {
 	assignvars $1 $2 $3 $4
-	t=`echo "$t/$d" | bc -l`
-	echo "-1*$c * ( sqrt(1 - $t*$t) - 1) + $b" | bc -l
+	t=`echo "$OPT $t/$d" | bc -l`
+	echo "$OPT -1*$c * ( sqrt(1 - $t*$t) - 1) + $b" | bc -l
 }
 function circ_easeOut {
 	assignvars $1 $2 $3 $4
-	t=`echo "($t/$d)-1" | bc -l`
-	echo "$c * sqrt(1 - $t*$t) + $b" | bc -l
+	t=`echo "$OPT ($t/$d)-1" | bc -l`
+	echo "$OPT $c * sqrt(1 - $t*$t) + $b" | bc -l
 }
 function circ_easeInOut {
 	assignvars $1 $2 $3 $4
-	t=`echo "$t/($d/2)" | bc -l`	
-	if [ `echo "$t < 1" | bc -l` -eq 1 ]
+	t=`echo "$OPT $t/($d/2)" | bc -l`	
+	if [ `echo "$OPT $t < 1" | bc -l` -eq 1 ]
 	then
-	  echo "-1*$c/2 * (sqrt(1 - $t*$t) - 1) + $b" | bc -l
+	  echo "$OPT -1*$c/2 * (sqrt(1 - $t*$t) - 1) + $b" | bc -l
 	else
-	  t=`echo "$t-2" | bc -l`
-	  echo "$c/2 * (sqrt(1 - $t*$t) + 1) + $b" | bc -l
+	  t=`echo "$OPT $t-2" | bc -l`
+	  echo "$OPT $c/2 * (sqrt(1 - $t*$t) + 1) + $b" | bc -l
 	fi	
 }
 
-
-# BACK =============================================================
+# BACK
+# -------------------------------------------------------------------------- #
 function back_easeIn {
 	assignvars $1 $2 $3 $4 $5
 	if [ -z "$s" ]
 	then
 	  s=$OVERSHOOT
 	fi
-	t=`echo "$t/$d" | bc -l`
-	echo "$c*$t*$t*(($s+1)*$t - $s) + $b" | bc -l
+	t=`echo "$OPT $t/$d" | bc -l`
+	echo "$OPT $c*$t*$t*(($s+1)*$t - $s) + $b" | bc -l
 }
 function back_easeOut {
 	assignvars $1 $2 $3 $4 $5
@@ -218,8 +222,8 @@ function back_easeOut {
 	then
 	  s=$OVERSHOOT
 	fi
-	t=`echo "($t/$d)-1" | bc -l`
-	echo "$c*($t*$t*(($s+1)*$t + $s) + 1) + $b" | bc -l
+	t=`echo "$OPT ($t/$d)-1" | bc -l`
+	echo "$OPT $c*($t*$t*(($s+1)*$t + $s) + 1) + $b" | bc -l
 }
 function back_easeInOut {
 	assignvars $1 $2 $3 $4 $5
@@ -227,173 +231,175 @@ function back_easeInOut {
 	then
 	  s=$OVERSHOOT
 	fi
-	t=`echo "$t/($d/2)" | bc -l`
-	s=`echo "$s*1.525" | bc -l`
-	if [ `echo "$t < 1" | bc -l` -eq 1 ]
+	t=`echo "$OPT $t/($d/2)" | bc -l`
+	s=`echo "$OPT $s*1.525" | bc -l`
+	if [ `echo "$OPT $t < 1" | bc -l` -eq 1 ]
 	then
-	  echo "$c/2*($t*$t*(($s+1)*$t - $s)) + $b" | bc -l
+	  echo "$OPT $c/2*($t*$t*(($s+1)*$t - $s)) + $b" | bc -l
 	else
-	  t=`echo "$t-2" | bc -l`
-	  echo "$c/2*($t*$t*(($s+1)*$t + $s) + 2) + $b" | bc -l
+	  t=`echo "$OPT $t-2" | bc -l`
+	  echo "$OPT $c/2*($t*$t*(($s+1)*$t + $s) + 2) + $b" | bc -l
 	fi
 }
 
-
-# ELASTIC =============================================================
+# ELASTIC
+# -------------------------------------------------------------------------- #
 function elastic_easeIn {
 	assignvars $1 $2 $3 $4 $5 $6
 	a=$s
-	TMP=`echo "$t/$d" | bc -l`
+	TMP=`echo "$OPT $t/$d" | bc -l`
 	if [ -z "$a" ]
 	then
 	  a=$c
 	fi
 	if [ -z "$p" ]
 	then
-	  p=`echo "$d * 0.3" | bc -l`
+	  p=`echo "$OPT $d * 0.3" | bc -l`
 	fi
-#-------------------------------------------------
-	if [ `echo "$t == 0" | bc -l` -eq 1 ]
+      #-------------------------------------------------
+	if [ `echo "$OPT $t == 0" | bc -l` -eq 1 ]
 	then
 	  echo $b
-	elif [ `echo "$TMP == 1" | bc -l` -eq 1 ]
+	elif [ `echo "$OPT $TMP == 1" | bc -l` -eq 1 ]
 	then
-	  echo "$b+$c" | bc -l
+	  echo "$OPT $b+$c" | bc -l
 	else
-	  if [ `echo "$a < ${c#-}" | bc -l` -eq 1 ]
+	  if [ `echo "$OPT $a < ${c#-}" | bc -l` -eq 1 ]
 	  then
-	    s=`echo "$p / 4" | bc -l`
+	    s=`echo "$OPT $p / 4" | bc -l`
 	  else
-	    cda=`echo "$c/$a" | bc -l`
+	    cda=`echo "$OPT $c/$a" | bc -l`
 	    ASIN=`asin $cda`
-	    s=`echo "$p/(2*$PI) * $ASIN " | bc -l`
+	    s=`echo "$OPT $p/(2*$PI) * $ASIN " | bc -l`
 	  fi
-	TMP=`echo "$TMP - 1" | bc -l`
-	echo "-1*($a * e(l(2) * (10*$TMP)) * s( ($TMP*$d-$s)*(2*$PI)/$p )) + $b" | bc -l
+	TMP=`echo "$OPT $TMP - 1" | bc -l`
+	echo "$OPT -1*($a * e(l(2) * (10*$TMP)) * s( ($TMP*$d-$s)*(2*$PI)/$p )) + $b" | bc -l
 	fi
 }
 function elastic_easeOut {
 	assignvars $1 $2 $3 $4 $5 $6
 	a=$s
-	TMP=`echo "$t/$d" | bc -l`
+	TMP=`echo "$OPT $t/$d" | bc -l`
 	if [ -z "$a" ]
 	then
 	  a=$c
 	fi
 	if [ -z "$p" ]
 	then
-	  p=`echo "$d * 0.3" | bc -l`
+	  p=`echo "$OPT $d * 0.3" | bc -l`
 	fi
-#-------------------------------------------------
-	if [ `echo "$t == 0" | bc -l` -eq 1 ]
+      #-------------------------------------------------
+	if [ `echo "$OPT $t == 0" | bc -l` -eq 1 ]
 	then
 	  echo $b
-	elif [ `echo "$TMP == 1" | bc -l` -eq 1 ]
+	elif [ `echo "$OPT $TMP == 1" | bc -l` -eq 1 ]
 	then
-	  echo "$b+$c" | bc -l
+	  echo "$OPT $b+$c" | bc -l
 	else
-	  if [ `echo "$a < ${c#-}" | bc -l` -eq 1 ]
+	  if [ `echo "$OPT $a < ${c#-}" | bc -l` -eq 1 ]
 	  then
-	    s=`echo "$p / 4" | bc -l`
+	    s=`echo "$OPT $p / 4" | bc -l`
 	  else
-	    cda=`echo "$c/$a" | bc -l`
+	    cda=`echo "$OPT $c/$a" | bc -l`
 	    ASIN=`asin $cda`
-	    s=`echo "$p/(2*$PI) * $ASIN " | bc -l`
+	    s=`echo "$OPT $p/(2*$PI) * $ASIN " | bc -l`
 	  fi
-	echo "$a * e(l(2) * (-10*$TMP)) * s( ($TMP*$d-$s)*(2*$PI)/$p ) +$c + $b" | bc -l
+	echo "$OPT $a * e(l(2) * (-10*$TMP)) * s( ($TMP*$d-$s)*(2*$PI)/$p ) +$c + $b" | bc -l
 	fi	
 }
 function elastic_easeInOut {
 	assignvars $1 $2 $3 $4 $5 $6
 	a=$s
-	TMP=`echo "$t/($d/2)" | bc -l`
+	TMP=`echo "$OPT $t/($d/2)" | bc -l`
 	if [ -z "$a" ]
 	then
 	  a=$c
 	fi
 	if [ -z "$p" ]
 	then
-	  p=`echo "$d * (0.3*1.5)" | bc -l`
+	  p=`echo "$OPT $d * (0.3*1.5)" | bc -l`
 	fi
-#-------------------------------------------------
-	if [ `echo "$t == 0" | bc -l` -eq 1 ]
+      #-------------------------------------------------
+	if [ `echo "$OPT $t == 0" | bc -l` -eq 1 ]
 	then
 	  echo $b
-	elif [ `echo "$TMP == 2" | bc -l` -eq 1 ]
+	elif [ `echo "$OPT $TMP == 2" | bc -l` -eq 1 ]
 	then
-	  echo "$b+$c" | bc -l
+	  echo "$OPT $b+$c" | bc -l
 	  
 	else
-	  if [ `echo "$a < ${c#-}" | bc -l` -eq 1 ]
+	  if [ `echo "$OPT $a < ${c#-}" | bc -l` -eq 1 ]
 	  then
-	    s=`echo "$p / 4" | bc -l`
+	    s=`echo "$OPT $p / 4" | bc -l`
 	  else
-	    cda=`echo "$c/$a" | bc -l`
+	    cda=`echo "$OPT $c/$a" | bc -l`
 	    ASIN=`asin $cda`
-	    s=`echo "$p/(2*$PI) * $ASIN " | bc -l`
+	    s=`echo "$OPT $p/(2*$PI) * $ASIN " | bc -l`
 	  fi
-	    if [ `echo "$TMP < 1" | bc -l` -eq 1 ]
+	    if [ `echo "$OPT $TMP < 1" | bc -l` -eq 1 ]
 	    then
-	      TMP=`echo "$TMP - 1" | bc -l`
-	      echo "-0.5*($a * e(l(2) * (10*$TMP)) * s( ($TMP*$d-$s)*(2*$PI)/$p )) + $b" | bc -l
+	      TMP=`echo "$OPT $TMP - 1" | bc -l`
+	      echo "$OPT -0.5*($a * e(l(2) * (10*$TMP)) * s( ($TMP*$d-$s)*(2*$PI)/$p )) + $b" | bc -l
 	    else
-	      TMP=`echo "$TMP - 1" | bc -l`
-	      echo "0.5*$a * e(l(2) * (-10*$TMP)) * s( ($TMP*$d-$s)*(2*$PI)/$p ) +$c + $b" | bc -l
+	      TMP=`echo "$OPT $TMP - 1" | bc -l`
+	      echo "$OPT 0.5*$a * e(l(2) * (-10*$TMP)) * s( ($TMP*$d-$s)*(2*$PI)/$p ) +$c + $b" | bc -l
 	    fi
 	fi
 }
 
-
-# BOUNCE =============================================================
+# BOUNCE
+# -------------------------------------------------------------------------- #
 function bounce_easeIn {
 	assignvars $1 $2 $3 $4
-	dt=`echo "$d-$t" | bc -l`
+	dt=`echo "$OPT $d-$t" | bc -l`
 	beo=`bounce_easeOut $dt 0 $c $d`
-	echo "$c - $beo + $b" | bc -l
+	echo "$OPT $c - $beo + $b" | bc -l
 }
 function bounce_easeOut {
 	assignvars $1 $2 $3 $4
-	t=`echo "$t/$d" | bc -l`
-	if [ `echo "$t < (1/2.75)" | bc -l` -eq 1 ]
+	t=`echo "$OPT $t/$d" | bc -l`
+	if [ `echo "$OPT $t < (1/2.75)" | bc -l` -eq 1 ]
 	then
-	  echo "$c*(7.5625*$t*$t) + $b" | bc -l
-	elif [ `echo "$t < (2/2.75)" | bc -l` -eq 1 ]
+	  echo "$OPT $c*(7.5625*$t*$t) + $b" | bc -l
+	elif [ `echo "$OPT $t < (2/2.75)" | bc -l` -eq 1 ]
 	then
-	  t=`echo "$t-(1.5/2.75)" | bc -l`
-	  echo "$c*(7.5625*$t*$t + 0.75) + $b" | bc -l
-	elif [ `echo "$t < (2.5/2.75)" | bc -l` -eq 1 ]
+	  t=`echo "$OPT $t-(1.5/2.75)" | bc -l`
+	  echo "$OPT $c*(7.5625*$t*$t + 0.75) + $b" | bc -l
+	elif [ `echo "$OPT $t < (2.5/2.75)" | bc -l` -eq 1 ]
 	then
-	  t=`echo "$t-(2.25/2.75)" | bc -l`
-	  echo "$c*(7.5625*$t*$t + 0.9375) + $b" | bc -l
+	  t=`echo "$OPT $t-(2.25/2.75)" | bc -l`
+	  echo "$OPT $c*(7.5625*$t*$t + 0.9375) + $b" | bc -l
 	else
-	  t=`echo "$t-(2.625/2.75)" | bc -l`
-	  echo "$c*(7.5625*$t*$t + 0.984375) + $b" | bc -l
+	  t=`echo "$OPT $t-(2.625/2.75)" | bc -l`
+	  echo "$OPT $c*(7.5625*$t*$t + 0.984375) + $b" | bc -l
 	fi
 }
 function bounce_easeInOut {
 	assignvars $1 $2 $3 $4
-	if [ `echo "$t < ($d/2)" | bc -l` -eq 1 ]
+	if [ `echo "$OPT $t < ($d/2)" | bc -l` -eq 1 ]
 	then
-	  t2=`echo "$t*2" | bc -l`
+	  t2=`echo "$OPT $t*2" | bc -l`
 	  bei=`bounce_easeIn $t2 0 $c $d`
-	  echo "$bei * 0.5 + $b" | bc -l
+	  echo "$OPT $bei * 0.5 + $b" | bc -l
 	else
-	  t2d=`echo "$t*2-$d" | bc -l`
+	  t2d=`echo "$OPT $t*2-$d" | bc -l`
 	  beo=`bounce_easeOut $t2d 0 $c $d`
-	  echo "$beo * 0.5 + $c * 0.5 + $b" | bc -l
+	  echo "$OPT $beo * 0.5 + $c * 0.5 + $b" | bc -l
 	fi
 }
 
 
+# HELPER
+# -------------------------------------------------------------------------- #
 function asin {
   x=$1
-  if [ `echo "$x == 1" | bc -l` -eq 1 ]
+  if [ `echo "$OPT $x == 1" | bc -l` -eq 1 ]
   then
-    echo "$PI/2" | bc -l
-  elif [ `echo "$x == -1" | bc -l` -eq 1 ]
+    echo "$OPT $PI/2" | bc -l
+  elif [ `echo "$OPT $x == -1" | bc -l` -eq 1 ]
   then
-    echo "-1*$PI/2" | bc -l
+    echo "$OPT -1*$PI/2" | bc -l
   else
-    echo "( a($x / sqrt(1-($x ^ 2))  ) )" | bc -l
+    echo "$OPT ( a($x / sqrt(1-($x ^ 2))  ) )" | bc -l
   fi
 }
